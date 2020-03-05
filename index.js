4d2b105278e07295aa2aad0a6377d91b7ae0d769
ilwanhusnadi <56053064+ilwanhusnadi@users.noreply.github.com>
2020-03-05 12:13:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: "Aplikasi pertama"
+    });
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit();
+        todayWindow = null;
+    })
+});
